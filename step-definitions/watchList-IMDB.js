d1acc7a692de674a554f361e47a1942ead017399
moona <memoona.tahira@gmail.com>
2020-05-12 19:23:52 +0200

    add empty definations

--- step-definitions/watchList-IMDB.js
+++ step-definitions/watchList-IMDB.js
@@ -2,5 +2,25 @@ const { username, password } = require('./credentials.json');
 let { $, sleep } = require('./funcs');
 
 module.exports = function () {
+  this.When(/^I am logged\-in$/, async function () {
+  });
+
+  this.When(/^search the movie$/, async function () {
+
+  });
+
+
+  this.When(/^add movie to watchlist$/, async function () {
+
+  });
+
+
+  this.Then(/^the movie will have green ribbon$/, async function () {
+
+  });
+
+  this.Then(/^I can log out$/, async function () {
+
+  });
 
 }
