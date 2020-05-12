34573c1b721e15c614c8fa0ebe542d6106c25f5c
moona <memoona.tahira@gmail.com>
2020-05-12 19:25:04 +0200

    adding defination

--- step-definitions/watchList-IMDB.js
+++ step-definitions/watchList-IMDB.js
@@ -2,7 +2,29 @@ const { username, password } = require('./credentials.json');
 let { $, sleep } = require('./funcs');
 
 module.exports = function () {
+
+  let sleepTime = 50;
+
   this.When(/^I am logged\-in$/, async function () {
+
+    let signInLink = await $('.imdb-header__signin-text');
+    await signInLink.click();
+    await sleep(sleepTime);
+
+    let sign_in = driver.findElement(By.linkText("Sign in with IMDb"));
+    await sign_in.click();
+    await sleep(sleepTime);
+
+    let ap_email = driver.findElement(By.id("ap_email"));
+    ap_email.sendKeys(username);
+    await sleep(sleepTime);
+
+    let ap_password = driver.findElement(By.id("ap_password"));
+    ap_password.sendKeys(password);
+    await sleep(sleepTime);
+
+    let signInSubmit = driver.findElement(By.id("signInSubmit"));
+    await signInSubmit.click();
   });
 
   this.When(/^search the movie$/, async function () {
