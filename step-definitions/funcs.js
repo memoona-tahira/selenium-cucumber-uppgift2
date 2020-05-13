70a8ca2b39a106b72e528ab6ae249cae83e28b1d
RevaZara <reva1zara1@hotmail.com>
2020-05-13 11:15:20 +0200

    alternativ från en select-dropdownlista

--- step-definitions/funcs.js
+++ step-definitions/funcs.js
@@ -1,16 +1,29 @@
 module.exports = {
-  $: async function (selector){
+  $: async function (selector) {
     let elements = await driver.findElements(by.css(selector));
-    if(elements.length === 0){
-      return null; 
+    if (elements.length === 0) {
+      return null;
     }
-    if(elements.length === 1){
+    if (elements.length === 1) {
       return elements[0];
     }
     return elements;
   },
-  sleep: function(ms) {
+  sleep: function (ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
 }
- 
+global.selectOption = async (cssSelector, optionName) => {
+  let codeToRun = `(()=>{
+      let select = document.querySelector('cssSelector');
+      if(!select){return;}
+      let options = select.querySelectorAll('option');
+      options.forEach((option,i) => {
+        option.innerText.trim() === 'optionName' && (select.selectedIndex = i);
+      })
+    })()`
+    .split('cssSelector').join(cssSelector)
+    .split('optionName').join(optionName);
+  await driver.executeScript(codeToRun);
+}
+
