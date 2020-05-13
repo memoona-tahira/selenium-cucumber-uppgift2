02aa9f18f7e81d14e51d08d943f2ccd43596939a
moona <memoona.tahira@gmail.com>
2020-05-13 10:49:26 +0200

    after working watchlist click

--- step-definitions/watchList-IMDB.js
+++ step-definitions/watchList-IMDB.js
@@ -81,4 +81,49 @@ module.exports = function () {
 
   });
 
+  this.When(/^have movie in watch list$/, async function () {
+    let searchField = await $('input[placeholder= "Search IMDb"]');
+    assert(searchField, 'Can not find the search field on the page');
+    searchField.sendKeys("moona");
+    await searchField.sendKeys(selenium.Key.ENTER);
+    await driver.wait(until.elementLocated(By.css('.findResult, .findNoResults')));
+
+    let results = await $('.findResult');
+    assert(results, 'Could not find any results');
+    let firstResult = results[0];
+
+    let resultText = await firstResult.getText();
+    var possibleName = resultText.substr(0, resultText.indexOf('('));
+    await sleep(sleepTime);
+    let firstResultLink = driver.findElement(By.linkText(possibleName.trim()));
+    await firstResultLink.click();
+    await sleep(sleepTime);
+    await driver.wait(until.elementLocated(By.css('.watchlist--title-main-desktop-standalone')));
+
+
+    let addWatchList = await $('.uc-add-wl-button-icon--add');
+    if (await addWatchList.isDisplayed()) {
+      await addWatchList.click();
+      await sleep(sleepTime);
+    }
+    await driver.wait(until.elementLocated(By.css('.inWL')));
+    let ribbon$ = await $('.inWL');
+    assert(ribbon$, 'Could not find any ribbon with inWL class');
+
+  });
+
+  this.When(/^I am on the watchlist page$/, async function () {
+    //let watchlistLink = await $('a[href^="/list/watchlist"]');
+    let watchlistLink = await $('a[href="/list/watchlist?ref_=nv_usr_wl_all_0"]');
+
+    await watchlistLink.click();
+    await sleep(sleepTime);
+  });
+
+
+ this.Then(/^the watchlist contain the movie$/, async function () {
+
+       });
+
+
 }
