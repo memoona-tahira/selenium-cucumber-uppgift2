7a4ec970152a2759d94c6710c5c332502f5e3a63
moona <memoona.tahira@gmail.com>
2020-05-13 16:51:20 +0200

    changes for scenario

--- step-definitions/watchList-IMDB.js
+++ step-definitions/watchList-IMDB.js
@@ -3,44 +3,42 @@ let { $, sleep } = require('./funcs');
 
 module.exports = function () {
 
-  let sleepTime = 50;
+  let sleepTime = 100;
+  let movieName = "Devdas";
 
   this.When(/^I am logged\-in$/, async function () {
 
-    let signInLink = await $('.imdb-header__signin-text');
+    let signInLink = await $('a[href="/registration/signin?ref=nv_generic_lgin"]');
     await signInLink.click();
-    await sleep(sleepTime);
 
     let sign_in = driver.findElement(By.linkText("Sign in with IMDb"));
     await sign_in.click();
     await sleep(sleepTime);
 
-    let ap_email = driver.findElement(By.id("ap_email"));
-    ap_email.sendKeys(username);
+    let userField = await $('input[id="ap_email"]');
+    userField.sendKeys(username);
     await sleep(sleepTime);
 
-    let ap_password = driver.findElement(By.id("ap_password"));
-    ap_password.sendKeys(password);
+    let passwordField = await $('input[id="ap_password"]');
+    passwordField.sendKeys(password);
     await sleep(sleepTime);
 
-    let signInSubmit = driver.findElement(By.id("signInSubmit"));
-    await signInSubmit.click();
+    let signInButton = await $('input[id="signInSubmit"]');
+    signInButton.click();
+
+    await sleep(sleepTime);
   });
-
 
   this.When(/^search the movie$/, async function () {
 
     let searchField = await $('input[placeholder= "Search IMDb"]');
     assert(searchField, 'Can not find the search field on the page');
-    searchField.sendKeys("moona");
+    searchField.sendKeys(movieName);
     await searchField.sendKeys(selenium.Key.ENTER);
   });
 
-
   this.When(/^add movie to watchlist$/, async function () {
-
     await driver.wait(until.elementLocated(By.css('.findResult, .findNoResults')));
-
     let results = await $('.findResult');
     assert(results, 'Could not find any results');
     let firstResult = results[0];
@@ -51,7 +49,7 @@ module.exports = function () {
     let firstResultLink = driver.findElement(By.linkText(possibleName.trim()));
     await firstResultLink.click();
     await sleep(sleepTime);
-    await driver.wait(until.elementLocated(By.css('.watchlist--title-main-desktop-standalone')));
+    await driver.wait(until.elementLocated(By.css('.summary_text')));
 
 
     let addWatchList = await $('.uc-add-wl-button-icon--add');
@@ -61,69 +59,36 @@ module.exports = function () {
     }
   });
 
-
   this.Then(/^the movie will have green ribbon$/, async function () {
     await driver.wait(until.elementLocated(By.css('.inWL')));
-    let ribbon$ = await $('.inWL');
-    assert(ribbon$, 'Could not find any ribbon with inWL class');
+    let ribbon = await $('.inWL');
+    assert(ribbon, 'Could not find any ribbon with inWL class');
   });
 
-  this.Then(/^I can log out$/, async function () {
+  this.When(/^I can log out$/, async function () {
     await sleep(sleepTime);
     let userMenu = await $('.navbar__user-menu-toggle__button');
     await userMenu.click();
     let sign_out = driver.findElement(By.linkText("Sign out"));
     await sign_out.click();
     await sleep(sleepTime);
-
-    let signInLink = await $('.imdb-header__signin-text');
-    assert.isNotNull(signInLink, 'Sign in link available again');
-
   });
 
-  this.When(/^have movie in watch list$/, async function () {
-    let searchField = await $('input[placeholder= "Search IMDb"]');
-    assert(searchField, 'Can not find the search field on the page');
-    searchField.sendKeys("moona");
-    await searchField.sendKeys(selenium.Key.ENTER);
-    await driver.wait(until.elementLocated(By.css('.findResult, .findNoResults')));
-
-    let results = await $('.findResult');
-    assert(results, 'Could not find any results');
-    let firstResult = results[0];
-
-    let resultText = await firstResult.getText();
-    var possibleName = resultText.substr(0, resultText.indexOf('('));
-    await sleep(sleepTime);
-    let firstResultLink = driver.findElement(By.linkText(possibleName.trim()));
-    await firstResultLink.click();
-    await sleep(sleepTime);
-    await driver.wait(until.elementLocated(By.css('.watchlist--title-main-desktop-standalone')));
-
-
-    let addWatchList = await $('.uc-add-wl-button-icon--add');
-    if (await addWatchList.isDisplayed()) {
-      await addWatchList.click();
-      await sleep(sleepTime);
-    }
-    await driver.wait(until.elementLocated(By.css('.inWL')));
-    let ribbon$ = await $('.inWL');
-    assert(ribbon$, 'Could not find any ribbon with inWL class');
-
+  this.Then(/^Login link is available$/, async function () {
+    let signInLink = await $('.imdb-header__signin-text');
+    assert.isNotNull(signInLink, 'Sign in link available again');
   });
 
   this.When(/^I am on the watchlist page$/, async function () {
-    //let watchlistLink = await $('a[href^="/list/watchlist"]');
     let watchlistLink = await $('a[href="/list/watchlist?ref_=nv_usr_wl_all_0"]');
-
     await watchlistLink.click();
     await sleep(sleepTime);
   });
 
-
- this.Then(/^the watchlist contain the movie$/, async function () {
-
-       });
-
-
+  this.Then(/^the watchlist contain the movie$/, async function () {
+    await driver.wait(until.elementLocated(By.css('.lister-list')));
+    await sleep(sleepTime);
+    let movieInList = driver.findElement(By.linkText(movieName));
+    assert.isNotNull(movieInList, 'Movie is not in the list');
+  });
 }
