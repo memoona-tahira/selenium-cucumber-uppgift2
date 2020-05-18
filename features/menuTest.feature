6d4865b12ffc55931d67dd66605be4eae9131338
RevaZara <reva1zara1@hotmail.com>
2020-05-18 09:36:28 +0200

    Senario 5.4 menu test

--- features/menuTest.feature
+++ features/menuTest.feature
@@ -12,4 +12,13 @@ Feature: menu test
     And select Most Popular TV Shows from TV Shows catogary
     And then i click on it
 
+  Scenario: As a user I want to see contain on the Celebs catogary
+    Given that I am on the IMDB site
+    When I click in the menu
+    Then I select Born today from Celebs catogary
+    And then i click in the menu
+    Then I select Most Popular Celebs from Celebs catogary
+    And I go to the menu igen
+    Then I select Celebrity News from Celebs catogary
+    And I ckick to the first news
 
--- step-definitions/menuTest.js
+++ step-definitions/menuTest.js
@@ -37,5 +37,46 @@ module.exports = function () {
     let result = await $('.titleColumn a');
     assert(result, 'Could not find the Movie');
     await result[result.length - 1].click();
+    await sleep(1000);
   });
+  //_________________________________________
+
+  this.Then(/^I select Born today from Celebs catogary$/, async function () {
+    let bornToday = await $('a[href="/feature/bornondate/?ref_=nv_cel_brn"]');
+    assert(bornToday, 'Could not find the link Born today');
+    await bornToday.click();
+  });
+
+  this.Then(/^then i click in the menu$/, async function () {
+    let menuButton = await $('#imdbHeader-navDrawerOpen--desktop');
+    assert(menuButton, 'Could not find the menu button');
+    menuButton.click();
+  });
+  this.Then(/^I select Most Popular Celebs from Celebs catogary$/, async function () {
+    let mostPopularCelebs = await $('a[href="https://www.imdb.com/search/name/?gender=male%2Cfemale&ref_=nv_cel_m"]');
+    assert(mostPopularCelebs, 'Could not find the link Most Popular Celebs');
+    await mostPopularCelebs.click();
+  });
+
+  this.Then(/^I go to the menu igen$/, async function () {
+    let menuButton = await $('#imdbHeader-navDrawerOpen--desktop');
+    assert(menuButton, 'Could not find the menu button');
+    menuButton.click();
+  });
+
+  this.Then(/^I select Celebrity News from Celebs catogary$/, async function () {
+    let celebrityNews = await $('a[href="/news/celebrity/?ref_=nv_cel_nw"]');
+    assert(celebrityNews, 'Could not find the link Celebrity News');
+    await celebrityNews.click();
+  });
+  this.Then(/^I ckick to the first news$/, async function () {
+    let result = await $('.news-article__title a');
+    assert(result, 'Could not find the Movie');
+    await result[0].click();
+    await sleep(1000);
+  });
+
+  //_______________________________________________________
+
+
 }
