d651151d6bdb9be57c51b828e4619aa219544f2b
RevaZara <reva1zara1@hotmail.com>
2020-05-23 23:14:02 +0200

    Verifiera dina test

--- features/menuTest.feature
+++ features/menuTest.feature
@@ -1,25 +1,23 @@
 Feature: menu test
   Scenario: As a user i want to see top rated movies and sorted by IMDB Rating
-   Given that I am on the IMDB site
-   When I click in the menu
+    Given that I am on the IMDB site
+    When I click in the menu
     And select Top Rated Movies from movies catogary
     Then It will show you Text Top Retad Movies with number of titles
-    And I will choose from sort by Release Date
+    And sorted by Release Date
 
- Scenario: As a user i want to see Most Popular TV Shows
+  Scenario: As a user i want to see Most Popular TV Shows
     Given that I am on the IMDB site
     When I click in the menu
     And select Most Popular TV Shows from TV Shows catogary
+    And sorted by IMDB Rating
     And then i click on it
 
   Scenario: As a user I want to see contain on the Celebs catogary
     Given that I am on the IMDB site
     When I click in the menu
     Then I select Born today from Celebs catogary
-    And then i click in the menu
-    Then I select Most Popular Celebs from Celebs catogary
-    And I go to the menu igen
-    Then I select Celebrity News from Celebs catogary
-    And I ckick to the first news
+    And sort by name ascending
+    Then sort by name descending
 
 
--- step-definitions/menuTest.js
+++ step-definitions/menuTest.js
@@ -20,10 +20,21 @@ module.exports = function () {
     let pageHeaderText = await pageHeader.getText();
     assert.equal(pageHeaderText, "Top Rated Movies", 'Incorrect page title... does not include Top Rated Movies');
   });
-  this.Then(/^I will choose from sort by Release Date$/, async function () {
+  this.Then(/^sorted by Release Date$/, async function () {
     await selectOption('#lister-sort-by-options', 'Release Date');
-    await sleep(1000);
+    await sleep(3000);
+
+    let releaseDateList = await $('.secondaryInfo');
+
+    for (let i = 0; i < releaseDateList.length - 1; i++) {
+      let releaseDate_1 = releaseDateList[i];
+      let releaseDate_1_text = await releaseDate_1.getText();
+      let releaseDate_2 = releaseDateList[i + 1];
+      let releaseDate_2_text = await releaseDate_2.getText();
+      assert(Number(releaseDate_1_text.slice(1, -1)) >= Number(releaseDate_2_text.slice(1, -1)), 'Not sorted by release date descending.');
+    }
   });
+
 
   //___________________________________
   this.When(/^select Most Popular TV Shows from TV Shows catogary$/, async function () {
@@ -31,6 +42,20 @@ module.exports = function () {
     assert(mostPopularShows, 'Could not find the link Most Popular TV Shows');
     await mostPopularShows.click();
     await sleep();
+  });
+  this.When(/^sorted by IMDB Rating$/, async function () {
+    await selectOption('#lister-sort-by-options', 'IMDb Rating');
+    await sleep(3000);
+
+    let iMDBRatingList = await $('.imdbRating > strong');
+
+    for (let i = 0; i < iMDBRatingList.length - 1; i++) {
+      let iMDBRating_1 = iMDBRatingList[i];
+      let iMDBRating_1_text = await iMDBRating_1.getText();
+      let iMDBRating_2 = iMDBRatingList[i + 1];
+      let iMDBRating_2_text = await iMDBRating_2.getText();
+      assert(Number(iMDBRating_1_text.replace(',', '.')) >= Number(iMDBRating_2_text.replace(',', '.')), 'Not sorted by IMDb Rating date descending.');
+    }
   });
 
   this.When(/^then i click on it$/, async function () {
@@ -47,33 +72,36 @@ module.exports = function () {
     await bornToday.click();
   });
 
-  this.Then(/^then i click in the menu$/, async function () {
-    let menuButton = await $('#imdbHeader-navDrawerOpen--desktop');
-    assert(menuButton, 'Could not find the menu button');
-    menuButton.click();
-  });
-  this.Then(/^I select Most Popular Celebs from Celebs catogary$/, async function () {
-    let mostPopularCelebs = await $('a[href="https://www.imdb.com/search/name/?gender=male%2Cfemale&ref_=nv_cel_m"]');
-    assert(mostPopularCelebs, 'Could not find the link Most Popular Celebs');
-    await mostPopularCelebs.click();
+  this.Then(/^sort by name ascending$/, async function () {
+    let ascending = await $('a[href="/search/name/?birth_monthday=05-23&sort=alpha,asc&ref_=rlm"]');
+    assert(ascending, 'Could not find sort by A-Z');
+    await ascending.click();
+    await sleep(300);
+    let nameList = await $('.lister-item-header > a');
+
+    for (let i = 0; i < nameList.length - 1; i++) {
+      let name_1 = nameList[i];
+      let name_1_text = await name_1.getText();
+      let name_2 = nameList[i + 1];
+      let name_2_text = await name_2.getText();
+      assert(name_1_text <= name_2_text, 'Not sorted by name ascending');
+    }
   });
 
-  this.Then(/^I go to the menu igen$/, async function () {
-    let menuButton = await $('#imdbHeader-navDrawerOpen--desktop');
-    assert(menuButton, 'Could not find the menu button');
-    menuButton.click();
-  });
+  this.Then(/^sort by name descending$/, async function () {
+    let descending = await $('a[href="/search/name/?birth_monthday=05-23&sort=alpha,desc&ref_=rlm"]');
+    assert(descending, 'Could not find sort by A-Z');
+    await descending.click();
+    await sleep(300);
+    let nameList = await $('.lister-item-header > a');
 
-  this.Then(/^I select Celebrity News from Celebs catogary$/, async function () {
-    let celebrityNews = await $('a[href="/news/celebrity/?ref_=nv_cel_nw"]');
-    assert(celebrityNews, 'Could not find the link Celebrity News');
-    await celebrityNews.click();
-  });
-  this.Then(/^I ckick to the first news$/, async function () {
-    let result = await $('.news-article__title a');
-    assert(result, 'Could not find the Movie');
-    await result[0].click();
-    await sleep(1000);
+    for (let i = 0; i < nameList.length - 1; i++) {
+      let name_1 = nameList[i];
+      let name_1_text = await name_1.getText();
+      let name_2 = nameList[i + 1];
+      let name_2_text = await name_2.getText();
+      assert(name_1_text >= name_2_text, 'Not sorted by name descending');
+    }
   });
 
   //_______________________________________________________
